577a64b474bb5c596b42af336710e8a31341a09a
ItsAlpara <bucher.fabian97@gmail.com>
2025-04-16 15:26:04 +0200

    After Lesson

--- Eigene Sprache/Lexer.py
+++ Eigene Sprache/Lexer.py
@@ -1,17 +1,52 @@
 from ply.lex import lex
 
 
+
+#reserved = {
+#   'if' : 'IF',
+#   'then' : 'THEN',
+#   'else' : 'ELSE',
+#   'while' : 'WHILE',
+#   ...
+#}
+#
+#
+#tokens = ['LPAREN','RPAREN',...,'ID'] + list(reserved.values())
+#def t_ID(t):
+#    r'[a-zA-Z_][a-zA-Z_0-9]*'
+#    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+#    return t
+
+
+
+reserved = {
+'and' : 'AND',
+'or'  : 'OR',
+'not' : 'NOT',
+'xor' : 'XOR',
+'xand' : 'XAND',
+'mod' : 'MODULO',
+}
+
+
+
+
 tokens = ['DECIMAL','HEX','BINARY','FLOAT','E'
          ,'COMPLEX','PLUS','MINUS','TIMES'
          ,'CEIL_DIVIDE','FLOOR_DIVIDE','DIVIDE'
-         ,'MODULO','LPAREN','RPAREN','PREFIX_MINUS'
+         ,'LPAREN','RPAREN','PREFIX_MINUS'
          ,'ABSOLUTE','POWER','POST_DEC','POST_INC'
          ,'GREATER_THEN','LESS_THEN','LESS_EQUAL','GREATER_EQUAL'
-         ,'EQUAL','NOT_EQUAL','ASSIGN','AND','OR','NOT','XOR','XAND',
-          'FALSE','TRUE','COMMENT','COMMENT_MULTILINE',]
+         ,'EQUAL','NOT_EQUAL','ASSIGN',
+          'FALSE','TRUE','COMMENT','COMMENT_MULTILINE',] + list(reserved.values())
 
 literals = [ '+','-','*','/' ]
 
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
 
 t_DECIMAL = r'\d+'
 t_HEX = r'0x'
@@ -31,7 +66,7 @@ t_CEIL_DIVIDE = r'\/'
 t_FLOOR_DIVIDE = r'\\'
 t_DIVIDE = r'\|'
 
-t_MODULO = r'mod'
+
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_PREFIX_MINUS = r'\-'
@@ -49,11 +84,7 @@ t_EQUAL = r'='
 t_NOT_EQUAL = r'!='
 t_ASSIGN = r'='
 
-t_AND = r'and'
-t_OR = r'or'
-t_NOT = r'not'
-t_XOR = r'xor'
-t_XAND = r'xand'
+
 
 t_FALSE = r'0'
 t_TRUE =  r'[a-zA-Z_][a-zA-Z0-9_]*]'
