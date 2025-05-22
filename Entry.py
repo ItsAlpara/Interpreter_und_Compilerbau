01639814f07bdbd64c6ad547082dece1115e4552
ItsAlpara <bucher.fabian97@gmail.com>
2025-05-22 14:18:12 +0200

    Entry added

---
+++ Entry.py
@@ -0,0 +1,38 @@
+class Entry:
+    pass
+
+class SymbolTable:
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.vars = {}
+
+    def push(self, names):
+        return SymbolTable(self).put(names)
+
+    def put(self, names):
+        for name in names:
+            if name not in self.vars:
+                self.vars[name] = Entry()
+        return self
+
+    def root(self):
+        t = self
+        while t.parent:
+            t = t.parent
+        return t
+
+    def __contains__(self, name):
+        if name in self.vars:
+            return True
+        elif self.parent is None:
+            return False
+        else:
+            return name in self.parent
+
+    def __getitem__(self, name):
+        if name in self.vars:
+            return self.vars[name]
+        elif self.parent is None:
+            raise KeyError(name)
+        else:
+            return self.parent[name]
